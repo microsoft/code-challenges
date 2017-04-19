e788fa2a41dc3322ebfce2df6345a9c3eae9b87b
matthew.batten@intergen.co.nz <matthew.batten@intergen.co.nz>
2017-04-19 12:16:59 +1200

    Added DocumentDb Webjob for tweets and updated configs

---
+++ Labs/DocumentDB/utilities/tweet-webjob/TweetTrackerWebJob/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+
+namespace TweetTrackerWebJob
+{
+    public class Program
+    {
+        // Please set the following connection strings in app.config for this WebJob to run:
+        // AzureWebJobsDashboard and AzureWebJobsStorage
+        static void Main()
+        {
+            var host = new JobHost();
+            host.CallAsync(typeof(Functions).GetMethod(nameof(Functions.ListenForTweets)));
+            host.RunAndBlock();
+        }
+    }
+}
