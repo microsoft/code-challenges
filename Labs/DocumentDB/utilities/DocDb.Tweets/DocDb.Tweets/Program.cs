0d0ec465b5a4d0b5abceb64db0c25882f0ab53a1
matthew.batten@intergen.co.nz <matthew.batten@intergen.co.nz>
2017-04-20 13:36:18 +1200

    Tweet uploader is now console app and app config is same for web

---
+++ Labs/DocumentDB/utilities/DocDb.Tweets/DocDb.Tweets/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure;
+using Tweetinvi;
+using Tweetinvi.Models;
+
+namespace DocDb.Tweets
+{
+    class Program
+    {
+        private static DocumentDbService _docDbService;
+
+        private static void Main()
+        {
+            Task.WaitAll(Task.Run(async () =>
+            {
+                var credentials = new TwitterCredentials(CloudConfigurationManager.GetSetting("Twitter:ConsumerKey"),
+                                                         CloudConfigurationManager.GetSetting("Twitter:ConsumerSecret"),
+                                                         CloudConfigurationManager.GetSetting("Twitter:AccessToken"),
+                                                         CloudConfigurationManager.GetSetting("Twitter:AccessTokenSecret"));
+
+                _docDbService = new DocumentDbService();
+                await _docDbService.Initalise();
+
+                var stream = Stream.CreateFilteredStream(credentials);
+
+                foreach (var tag in CloudConfigurationManager.GetSetting("Twitter:Tags").Split(','))
+                {
+                    stream.AddTrack(tag, async (tweet) =>
+                    {
+                        try
+                        {
+                            await _docDbService.UploadDocument(tweet);
+                            Console.WriteLine(tweet.FullText);
+                        }
+                        catch
+                        {
+                            //Ignore
+                        }
+                    });
+                }
+
+                stream.StartStreamMatchingAllConditions();
+
+            }));
+        }
+    }
+}
--- Labs/DocumentDB/utilities/DocDb.Tweets/DocDb.Tweets/Properties/AssemblyInfo.cs
+++ Labs/DocumentDB/utilities/DocDb.Tweets/DocDb.Tweets/Properties/AssemblyInfo.cs
@@ -2,34 +2,35 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TweetTrackerWebJob")]
+[assembly: AssemblyTitle("docdb-tweets")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TweetTrackerWebJob")]
+[assembly: AssemblyProduct("docdb-tweets")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("18580c1e-3135-425c-a05b-759f8a392098")]
+[assembly: Guid("1b287d58-0805-481e-85c6-85e6a109d1d2")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
