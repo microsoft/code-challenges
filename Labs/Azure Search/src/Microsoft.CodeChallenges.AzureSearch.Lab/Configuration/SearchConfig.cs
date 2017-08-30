c4128d4575b9d8c7c017e8caa9421c15b3743dce
tarasha <tarasha@microsoft.com>
2017-08-30 17:24:39 -0500

    Cloing All the Labs

--- Labs/Azure Search/src/Microsoft.CodeChallenges.AzureSearch.Lab/Configuration/SearchConfig.cs
+++ Labs/Azure Search/src/Microsoft.CodeChallenges.AzureSearch.Lab/Configuration/SearchConfig.cs
@@ -2,7 +2,7 @@
 {
     public static class SearchConfig
     {
-        public const string SearchName = "";
-        public const string SearchApiKey = "";
+        public const string SearchName = "https://azsearch-build-bak.search.windows.net";
+        public const string SearchApiKey = "FCAF9728436469CBF9A6F5F36A483EF0";
     }
 }
